7143504aee807198e14d8be450f42a2a43f2d85e
Fidelma <fidelma.beagan@gmail.com>
2019-09-26 14:38:04 +0100

    initial commit

---
+++ API_RestaurantBookings/src/test/java/com/codeclan/restaurant_bookings/RestaurantBookings/RestaurantBookingsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeclan.restaurant_bookings.RestaurantBookings;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantBookingsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
