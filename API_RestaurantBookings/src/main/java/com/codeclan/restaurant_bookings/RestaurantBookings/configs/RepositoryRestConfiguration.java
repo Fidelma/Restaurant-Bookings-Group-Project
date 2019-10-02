3c3c88bc8e1991137da9b12421984cb85c26d8d6
Fidelma <fidelma.beagan@gmail.com>
2019-10-02 18:05:14 +0100

    removed backend extra fails

--- API_RestaurantBookings/src/main/java/com/codeclan/restaurant_bookings/RestaurantBookings/configs/RepositoryRestConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.codeclan.restaurant_bookings.RestaurantBookings.configs;
-
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-public class RepositoryRestConfiguration {
-    public CorsRegistry getCorsRegistry() {
-    }
-}
